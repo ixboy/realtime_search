dcbe2f1991f7194cc9688046b2242955e50db3e7
ixboy <ismaelixboy@gmail.com>
2022-12-09 06:37:59 +0100

    config search model, add validations and helpers

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,8 @@ class Search < ApplicationRecord
   validates :query, length: { minimum: 3 }
   validates :searcher_id, presence: true
 
+  scope :order_by_most_searched, -> { group(:query).pluck('query, count(query) as COUNT').sort_by { |a| a[1] }.reverse }
+
   def searched?(search)
     require 'fuzzystringmatch'
     jarow = FuzzyStringMatch::JaroWinkler.create(:native)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
+  get 'analytics', to: 'searches#index'
 
   resources :articles
 end
