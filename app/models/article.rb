4fb1d1e113553aa96d21bdd5bc251e364ff26402
ixboy <ismaelixboy@gmail.com>
2022-12-09 06:35:13 +0100

    add validations and helper methods to models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 2, maximum: 100,
+                                              too_long: '100 characters is the maximum allowed' }
+  validates :body, presence: true, length: { minimum: 2, maximum: 3000,
+                                             too_long: '3000 characters is the maximum allowed' }
+  validates :user_id, presence: true
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
+
+  validates :email, presence: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }, uniqueness: true
+
+  validates :password, length: { minimum: 6 }, presence: true
+  validates :password_confirmation, length: { minimum: 6 }, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
